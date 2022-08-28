5ac458806dd6f51e39656ee847ed9ad46ccfd6a7
Abhi Garg <abhi666g@gmail.com>
2022-08-28 23:31:27 +0530

    [BMS] Added Region entity

---
+++ src/main/java/com/abhigarg/bookmyshow/entities/Region.java
@@ -0,0 +1,38 @@
+package com.abhigarg.bookmyshow.entities;
+
+import lombok.Data;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "Regions")
+@Data
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "NAME")
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @Column(name = "REGION_TYPE")
+    private int regionType;
+
+    @Column(name = "PARENT_ID")
+    private int parentId;
+
+    @Column(name = "CREATED_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Nullable
+    private Date createdAt;
+
+    @Column(name = "UPDATED_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Nullable
+    private Date updatedAt;
+}
